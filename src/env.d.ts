d0eceb40668df2c6c56b32c2c07c666792b2dedc
Dominic Fuchs <dominic.fuchs@aurum-consulting.de>
2025-12-01 15:11:21 +0100

    Add needed project structure

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,9 @@
+/// <reference types="astro/client" />
+
+interface Window {
+    Office: import("office-js");
+}
+
 declare namespace App {
     interface Locals {
         session: import("@/auth/models/session.model").Session;
